419f6967b6c72d12e75e0f90a69384f767203efc
Rashidi Zin <rashidi@zin.my>
2020-03-11 13:29:56 +0800

    Implement integration test with MySQLContainer
    MySQLContainer, offered by TestContainers allow simulation of
    production environment whereby MySQL is being used as datasource as
    compared to typical implementation which uses H2 as datasource for
    integration test and MySQL for production.
    
    With that we can ensure that all syntax and column names will not
    break our datasource.

---
+++ src/test/java/scratches/tc/domain/BookRepositoryRestResourceTests.java
@@ -0,0 +1,66 @@
+package scratches.tc.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import scratches.tc.configuration.DatasourceContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.CREATED;
+
+/**
+ * @author Rashidi Zin
+ */
+@SpringBootTest(properties = {
+        "spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver",
+        "spring.datasource.url=jdbc:tc:mysql:8:///demo",
+        "spring.jpa.generate-ddl=true"
+},
+        webEnvironment = RANDOM_PORT)
+public class BookRepositoryRestResourceTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private DatasourceContainer datasource = new DatasourceContainer().withDatabaseName("demo");
+
+    @BeforeEach
+    void setup() {
+        datasource.start();
+    }
+
+    @Test
+    @DisplayName("Entity will be created if datasource is available")
+    void create() {
+        var author = author();
+
+        var book = book(author);
+
+        ResponseEntity<Book> response = restTemplate.postForEntity("/books", book, Book.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(CREATED);
+    }
+
+    private Author author() {
+        var author = new Author();
+
+        author.setName("Rudyard Kipling");
+
+        return author;
+    }
+
+    private Book book(final Author author) {
+        var book = new Book();
+
+        book.setAuthor(author);
+        book.setTitle("The Jungle Book");
+
+        return book;
+    }
+
+}
